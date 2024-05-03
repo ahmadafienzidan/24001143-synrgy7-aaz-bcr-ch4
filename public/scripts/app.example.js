244e9f978a0fd306a63159a9ad3478525a1d0f27
Ahmad 'Afien Zidan <afienzidan227@gmail.com>
2024-05-03 21:45:15 +0700

    fix: feature

--- public/scripts/app.example.js
+++ public/scripts/app.example.js
@@ -1,71 +1,51 @@
-// import Car from "./car.example";
-// import Binar from "./binar";
+// Class App
 class App {
   constructor() {
     this.clearButton = document.getElementById("clear-btn");
     this.loadButton = document.getElementById("load-btn");
     this.carContainerElement = document.getElementById("cars-container");
-    this.dateInput = document.getElementById("date");
-    this.timeSelect = document.getElementById("time");
   }
 
   async init() {
-    // console.log(this.dateInput.value);
     await this.load();
-
+    console.log("Berhasil Async Init");
     // Register click listener
-    this.clearButton.onclick = this.clear;
+    // this.clearButton.onclick = this.clear;
     this.loadButton.onclick = this.run;
-    // this.loadButton.onclick = this.searchCars;
   }
 
   run = () => {
+    console.log("SUCCESS");
     this.clear();
     const data = this.filterCar();
 
-    // Jika data yang di cari tidak ditemukan
-    if (data.length === 0 || data === undefined) {
+    data.forEach((car) => {
+      console.log("BERJALAN ");
       const node = document.createElement("div");
-      node.innerHTML = `<div class="mt-2" role="alert">Data Tidak Ditemukan</div>`;
+      node.innerHTML = car.render();
       this.carContainerElement.appendChild(node);
-    } else {
-      // jika ditemukan
-      data.forEach((car) => {
-        const node = document.createElement("div");
-        node.innerHTML = car.render();
-        this.carContainerElement.appendChild(node);
-      });
-    }
+    });
   };
 
   filterCar() {
-    // Sesuaikan dengan kebutuhan Anda
-    // Contoh: Ambil nilai dari input driver, date, time, dan passenger
-    const driver = document.getElementById("driver").value;
-    const date = document.getElementById("date").value;
-    const time = document.getElementById("time").value;
-    const dateTime = new Date(`${date} ${time}`);
-    const passenger = document.getElementById("passenger").value;
+    console.log("FILTER BERHASIL");
+    let driver = document.getElementById("driver").value;
+    let date = document.getElementById("date").value;
+    let time = document.getElementById("time").value;
+    let passenger = document.getElementById("passenger").value;
+    let dateTime = `${date}T${time}.563Z`;
 
-    console.log("Driver:", driver); // Tambahkan ini untuk memeriksa nilai driver
-    console.log("Date:", date); // Tambahkan ini untuk memeriksa nilai tanggal
-    console.log("Time:", time); // Tambahkan ini untuk memeriksa nilai waktu
-    console.log("Penumpang:", passenger); // Tambahkan ini untuk memeriksa nilai jumlah penumpang
+    console.log(driver);
+    console.log(date);
+    console.log(time);
+    console.log(passenger);
+    console.log(dateTime);
 
-    // Proses filter
-    // Sesuaikan dengan kebutuhan Anda
-    // Contoh: Filter berdasarkan kondisi tertentu
-    if (driver === undefined || driver === "") {
-      alert("Please select a driver");
+    if (date == undefined && time == undefined) {
+      alert("Masukkan Input");
       return;
-    } else if (passenger == "" && driver.toString() == "true") {
-      return Car.list.filter((car) => car.available === true && car.availableAt <= dateTime);
-    } else if (passenger != "" && driver.toString() == "true") {
-      return Car.list.filter((car) => car.available === true && car.capacity >= passenger && car.availableAt <= dateTime);
-    } else if (passenger == "" && driver.toString() == "false") {
-      return Car.list.filter((car) => car.available === false && car.availableAt <= dateTime);
-    } else if (passenger != "" && driver.toString() == "false") {
-      return Car.list.filter((car) => car.available === false && car.capacity >= passenger && car.availableAt <= dateTime);
+    } else if (passenger >= 1) {
+      return Car.list.filter((car) => car.available === true && car.availableAt != dateTime && car.capacity >= passenger);
     }
   }
 
@@ -83,19 +63,4 @@ class App {
       child = this.carContainerElement.firstElementChild;
     }
   };
-}
-function formatRupiah(number) {
-  return new Intl.NumberFormat("id-ID", {
-    style: "currency",
-    currency: "IDR",
-  }).format(number);
-}
-
-// function format time date
-function getDateTimeNow() {
-  var today = new Date();
-  var date = today.getFullYear() + "-" + String(today.getMonth() + 1).padStart(2, "0") + "-" + String(today.getDate()).padStart(2, "0");
-  var time = String(today.getHours()).padStart(2, "0") + ":" + String(today.getMinutes()).padStart(2, "0") + ":" + String(today.getSeconds()).padStart(2, "0");
-  var dateNow = date + "T" + time + ".000Z";
-  return dateNow;
 }
